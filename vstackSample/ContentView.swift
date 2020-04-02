823d8d0aa044ad683cfc845e4ad09cf7ac321fbb
Miguel Tomairo <matomairo@gmail.com>
2020-04-02 15:49:26 -0500

    add file

--- vstackSample/ContentView.swift
+++ vstackSample/ContentView.swift
@@ -9,8 +9,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var countries = ["Peru","Bolivia","Argentina","Brazil","Chile","Colombia","Ecuador","Paraguay","Uruguay","Venezuela"].shuffled()
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var score = 0
+    @State private var alertTitlte = ""
+    @State private var showingAlert = false
+    
     var body: some View {
-        Text("Hello, World!")
+
+        NavigationView{
+            VStack{
+                ForEach(0...2, id: \.self) { number in
+                    Image(self.countries[number])
+                        .border(Color.black, width: 1)
+                        .onTapGesture {
+                            self.flagTapped(number)
+                    }
+                }
+            }.navigationBarTitle(Text(countries[correctAnswer].uppercased()))
+                .alert(isPresented: $showingAlert) { () -> Alert in
+                    Alert(title: Text(alertTitlte), message: Text("Tu puntuacion es \(score)"), dismissButton: .default(Text("Continuar")){
+                        self.askQuestion()
+                        })
+            }
+        }
+
+    }
+    
+    func flagTapped(_ tag: Int) {
+        if tag == correctAnswer {
+            self.score += 1
+            alertTitlte = "correcto"
+        }else {
+            self.score -= 1
+            alertTitlte = "incorrecto"
+        }
+        
+        showingAlert = true
+    }
+    
+    func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
